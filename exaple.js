184a6af9987b31ed105ab81c72d0a2d81d237ce7
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-22 00:43:53 +0600

    first commit

---
+++ exaple.js
@@ -0,0 +1,10 @@
+const name = 15;
+const student = {
+    name: 'jolil', friend: 'borsha', job: 'busniss',
+    makeMovie: function (name) {
+        console.log(name)
+    }
+
+
+}
+student.makeMovie('din ta day')
